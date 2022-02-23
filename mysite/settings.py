c6c7c654ae877d2f2ad626ccc5540e24bfa9d299
Rafał Mizera <62344859+rafal-mizera@users.noreply.github.com>
2022-02-23 14:13:12 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mysterious-reef-76616.herokuapp.com",'127.0.0.1']
 
 
 # Application definition
